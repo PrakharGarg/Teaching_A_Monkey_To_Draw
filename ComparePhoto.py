13dfd8431ac58dee28c108f7a5d89c792728120b
PrakharGarg <prakhargarg@utexas.edu>
2017-02-20 14:15:01 -0600

    comparing two photos now

--- ComparePhoto.py
+++ ComparePhoto.py
@@ -7,26 +7,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-def mse(imageA, imageB):
-	# the 'Mean Squared Error' between the two images is the
-	# sum of the squared difference between the two images;
-	# NOTE: the two images must have the same dimension
-	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-	err /= float(imageA.shape[0] * imageA.shape[1])
-	
-	# return the MSE, the lower the error, the more "similar"
-	# the two images are
-	return err
-
 def compare_images(imageA, imageB, title):
 	# compute the mean squared error and structural similarity
 	# index for the images
-	m = mse(imageA, imageB)
 	s = ssim(imageA, imageB)
 
 	# setup the figure
 	fig = plt.figure(title)
-	plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
+	plt.suptitle("SSIM: %.2f" % s)
 
 	# show first image
 	ax = fig.add_subplot(1, 2, 1)
@@ -43,18 +31,17 @@ def compare_images(imageA, imageB, title):
 
 # load the images -- the original, the original + contrast,
 # and the original + photoshop
-original = cv2.imread("images/jp_gates_original.png")
-contrast = cv2.imread("images/jp_gates_contrast.png")
-shopped = cv2.imread("images/jp_gates_photoshopped.png")
+original = cv2.imread("images/lisa.png")
+compare = cv2.imread("images/lisa.png")
+
 
 # convert the images to grayscale
 original = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
-contrast = cv2.cvtColor(contrast, cv2.COLOR_BGR2GRAY)
-shopped = cv2.cvtColor(shopped, cv2.COLOR_BGR2GRAY)
+compare = cv2.cvtColor(compare, cv2.COLOR_BGR2GRAY)
 
 # initialize the figure
 fig = plt.figure("Images")
-images = ("Original", original), ("Contrast", contrast), ("Photoshopped", shopped)
+images = ("Original", original), ("Compare", compare)
 
 # loop over the images
 for (i, (name, image)) in enumerate(images):
@@ -69,5 +56,4 @@ plt.show()
 
 # compare the images
 compare_images(original, original, "Original vs. Original")
-compare_images(original, contrast, "Original vs. Contrast")
-compare_images(original, shopped, "Original vs. Photoshopped")
+compare_images(original, compare, "Original vs. Compare")
